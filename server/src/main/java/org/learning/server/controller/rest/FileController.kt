556aa7353b3568a0d317290cfb1cb43d83af8361
WCF <1533144693@qq.com>
2021-01-13 22:08:32 +0800

    请cht进行code review

--- server/src/main/java/org/learning/server/controller/rest/FileController.kt
+++ server/src/main/java/org/learning/server/controller/rest/FileController.kt
@@ -1,17 +1,20 @@
 package org.learning.server.controller.rest
 
+import org.learning.server.model.annotation.NoLogin
 import org.learning.server.model.common.Response
 import org.learning.server.model.common.Responses
 import org.learning.server.service.impl.FileService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType.IMAGE_JPEG_VALUE
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import javax.servlet.http.HttpServletResponse
 
-@RestController
+@Controller
 @RequestMapping("api/file")
 class FileController {
     @Value("\${spring.servlet.multipart.location}")
@@ -21,10 +24,30 @@ class FileController {
     lateinit var fileService: FileService
 
     @PostMapping("/upload/img")
+    @ResponseBody
     fun upload(@RequestParam("file") file: MultipartFile): Response<String> {
         if (file.isEmpty) {
             return Responses.fail("文件不能为空")
         }
-        return Responses.ok(data = fileService.executeUpload(file,"img", uploadDir))
+        return Responses.ok(data = fileService.executeUpload(file, "img", uploadDir))
+    }
+
+    @GetMapping("/img/{name}", produces = [IMAGE_JPEG_VALUE])
+    @NoLogin
+    fun getImage(@PathVariable name: String, response: HttpServletResponse) {
+        println(name)
+        val image = File("$uploadDir/img", name)
+        response.contentType = "image/jpeg"
+        if (image.exists() && image.isFile) {
+            response.outputStream.write(image.readBytes())
+        } else {
+            response.outputStream.write(ClassPathResource("static/img/mockup5.jpg").inputStream.readAllBytes())
+        }
+    }
+
+    @GetMapping("/img/course/{name}", produces = [IMAGE_JPEG_VALUE])
+    @NoLogin
+    fun getCourseImage(@PathVariable name: String, response: HttpServletResponse) {
+        response.outputStream.write(ClassPathResource("static/img/course/$name").inputStream.readAllBytes())
     }
 }
